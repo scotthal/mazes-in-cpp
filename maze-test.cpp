7a2dfecaaf2c1f7bfc93b845eeb8f90e5d081629
Scott Hallock <scott@hallocks.net>
2025-02-12 11:30:34 -0700

    Add the beginnings of a Maze test

---
+++ maze-test.cpp
@@ -0,0 +1,21 @@
+#include "gtest/gtest.h" 
+
+#include "cell.h"
+#include "maze.h"
+
+TEST(MazeTest, HappyWidthHeight) {
+  constexpr int width = 3;
+  constexpr int height = 5;
+  Maze maze = Maze(width, height);
+  EXPECT_EQ(maze.width(), width);
+  EXPECT_EQ(maze.height(), height);
+  EXPECT_EQ(maze.size(), width * height);
+
+  for (int y = 0; y < height; ++y) {
+    for (int x = 0; x < width; ++x) {
+      Cell cell = maze.cell_at(x, y);
+      EXPECT_EQ(cell.x(), x);
+      EXPECT_EQ(cell.y(), y);
+    }
+  }
+}
